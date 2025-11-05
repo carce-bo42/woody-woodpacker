f8532b2ba7d39376b5d94c27b15e41b5601ce388
Christian Arce <yerildtt@gmail.com>
2025-11-06 00:38:38 +0100

    Beep Beep Bop

--- pruebas/encrypt.s
+++ pruebas/encrypt.s
@@ -1,5 +1,8 @@
 
 global _start
+
+#https://math.hws.edu/eck/cs220/f22/registers.html
+#https://stackoverflow.com/questions/2030366/what-do-the-brackets-mean-in-nasm-syntax-for-x86-asm
 _start:
 
     mov rax, 1               ; syscall write
@@ -8,38 +11,43 @@ _start:
     mov rdx, 10              ; len("..WOODY..\n")
     syscall
 
-    ; Tip: podemos usar el stack pero tendremos que devolverlo a donde estaba
-    ; al acabar: ese valor es importantísimo porque apunta a argc !
-    push rsp
-    mov rbp, rsp
+    lea rcx, [rel _start];      ; load address of the shellcode
+    mov rbx, [rel _data + 4*8]  ; vaddr of the shellcode
+    sub rcx, rbx                ; offset incorporado por el loader (VLA)
 
-    mov rdi, [rel _data + 2*8] ; ciphertext start address
-    mov rsi, [rel _data + 3*8] ; length of ciphertext
-    mov rax, [rel _data + 4*8] ; key
+    ; add the offset to the start of the encrypted .text section
+    mov rbx, [rel _dat + 2*8]
+    add rbx, rcx
 
-    mov rsp, rbp
-    pop rbp
+; First decryption byte is [rbx].
+_decryption_prepare:
 
-_loop_qword:
-    test    rsi, rsi          ; if rsi == 0 -> finished
-    jz      .end_qword
+    ; keep rbx with the value pointing towards the start
+    mov rsi, rbx
 
-    xor     qword [rdi], rax  ; XOR 8 bytes at [rdi] with rax
-    add     rdi, 8            ; advance pointer by 8
-    add     rax, 8
-    dec     rsi               ; rsi--
-    jmp     _loop_qword
+    ; use rdx as the end of iteration
+    mov r8, rsi
+    add r8, [rel _data + 3*8]
 
-.end_qword:
-    ; aquí rsi == 0
+    ; load key. We use an address here
+    lea rcx, [rel _data + 4*8]
+    mov rax, rcx ; save initial value to know when we need to loop the key again
 
+
+_loop_step:
+    cmp rsi, rdx
+    je ._done
+
+_decrypt_loop:
+    xor byte [rsi], [rcx]
 
 _data:
 	db "....WOODY....", 0x0a    ; 15 bytes
     db 0x0                      ; 1 byte for alignent
-    dq 0x1111111111111111       ; cipertext start address
-    dq 0x2222222222222222       ; cipertext size
-    times 32 db 0x33            ; key
+    dq 0x1111111111111111       ; cipertext start address  => 2*8
+    dq 0x2222222222222222       ; cipertext size           => 3*8
+    dq 0x3333333333333333       ; vaddr of the shellcode   => 4*8
+    times 32 db 0x44            ; key
 
 ; https://stackoverflow.com/questions/41912684/what-is-the-purpose-of-the-rbp-register-in-x86-64-assembler
 ; RBP es el registro que se utiliza en las funciones para que suceda que el stack se "acumule" en las llamadas
